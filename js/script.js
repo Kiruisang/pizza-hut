7d66e028dee03c238e63dd52dd60456dbefd6054
Kirui sang <brian.kirui@student.moringaschool.com>
2021-06-25 18:45:31 +0300

    added the final bill of the total pizza and the delivery message

--- js/script.js
+++ js/script.js
@@ -162,4 +162,27 @@ $(document).ready(function(){
     $("button#final-order").click(function(event){
       event.preventDefault();
 
-     
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliveryamount= checkoutTotal+100;
+      console.log("Final Bill is: "+deliveryamount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+  
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliveryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+   event.preventDefault();
+  });
+});
